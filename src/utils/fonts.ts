ffc3ae273c6d935c14b2af0a5aa46de5628d6a36
qoribulmujib <muhamadqoribulmujib@gmail.com>
2025-04-21 21:40:58 +0700

    ➕ add dependencies and change fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,7 +1,12 @@
 export const PlayfairDisplay = {
-    regular: 'PlayfairDisplay-Regular',
-    medium: 'PlayfairDisplay-Medium',
-    semiBold: 'PlayfairDisplay-SemiBold',
-    bold: 'PlayfairDisplay-Bold',
-    extraBold: 'PlayfairDisplay-ExtraBold',
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+    extraBold: 'Poppins-ExtraBold',
 };
+
+export const Monserrat = {
+    regular: 'Montserrat-Italic-VariableFont_wght',
+    medium: 'Montserrat-VariableFont_wght',
+}
